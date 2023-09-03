94bf288cce357046de7c64fef351972118093dc7
Kevin Killion <65492956+kevkillion@users.noreply.github.com>
2023-09-03 20:55:14 +1000

    Add files via upload

--- SQL_Join_vFinal.sql
+++ SQL_Join_vFinal.sql
@@ -1,36 +1,33 @@
 /* 
+-------------------------------------------------------------------------------------------------
 
-JOINING DATA
+This project joins the Customer Account and Customer Sales datasets using fuzzy match algorithym 
+LEVENSCHTEIN function.
+Step 1. Load in datasets
+Step 2. Set up datasets
+Step 3. Load LEVENSCHTEIN function
+Step 4. Test function
+Step 5. Join datasets using function
 
-Version: F
-Owner: KK
-
-Account Column List 
-    CUSTOMER_NUMBER, CUSTOMER_FIRST_NAME, CUSTOMER_LAST_NAME, DATE_OF_BIRTH, GENDER, STREET_ADDRESS,
-	COUNTRY, COUNTRY_CODE, EMAIL, INTEREST_TYPE, INTEREST_RATE, GROSS_BALANCE, INTEREST_BALANCE, 
-	PRINCIPLE_BALANCE, PROPERTY_OWNER, PROPERTY_VALUE, PROPERTY_YEAR,
-
-Sales Column List
-	CUSTOMER_NAME, SALE, SALES_SCHEME_CODE, SALES_STRATEGY_TYPE, MARITAL_STATUS, 
-	DAYS_SINCE_CUSTOMER_ENGAGEMENT, REG_DATE, START_DATE, OCC_CODE, TOTAL_HOUSEHOLD_ANNUAL_EXPENDITURE,
-	GROCERY_WEEKLY_SPENDING, TOTAL_EXPENDATURE_TO_DATE, NO_ITEMS_PURCHASED, WEEKLY_FUEL_EXPENDATURE,
-	SALES_CALL_STATUS, STORES_VISITED, LOYALTY_CARD_STATUS, LOYALTY_GIFT_SENT,
-
-
+--------------------------------------------------------------------------------------------------
 */
+
+
+--------------------STEP 1. IMPORT DATASETS--------------------
+
 Drop Table dbo.Customer_Account
 
 CREATE TABLE Customer_Account (
-	CUSTOMER_NUMBER�Varchar(55) ,
-	CUSTOMER_FIRST_NAME�Varchar(55)�,
-	CUSTOMER_LAST_NAME�Varchar(55)�,
-	DATE_OF_BIRTH�Date�,
-	GENDER�Varchar(25) null�,
-	STREET_ADDRESS�Varchar(55)�,
-	COUNTRY�Varchar(55)�,
-	COUNTRY_CODE�Varchar(55)�,
-	EMAIL�Varchar(55) null�,
-	INTEREST_TYPE�Varchar(55)�,
+	CUSTOMER_NUMBER Varchar(55) ,
+	CUSTOMER_FIRST_NAME Varchar(55) ,
+	CUSTOMER_LAST_NAME Varchar(55) ,
+	DATE_OF_BIRTH Date ,
+	GENDER Varchar(25) null ,
+	STREET_ADDRESS�Varchar(55) ,
+	COUNTRY Varchar(55) ,
+	COUNTRY_CODE Varchar(55) ,
+	EMAIL�Varchar(55) null ,
+	INTEREST_TYPE Varchar(55)�,
 	INTEREST_RATE�Float�,
 	GROSS_BALANCE�DECIMAL(30,2)�,
 	INTEREST_BALANCE�DECIMAL(30,2)�,
@@ -48,8 +45,8 @@ WITH (
 	FIRSTROW = 2 
 	) ;
 
-SELECT * FROM Customer_Account;
---------------------------------------
+--SELECT * FROM Customer_Account;
+-----------------------------------------------------------------
 
 Drop Table dbo.Customer_Sales
 
@@ -82,7 +79,10 @@ WITH (
 	FIRSTROW = 2 
 	) ;
 
-SELECT * FROM Customer_Sales ;
+--SELECT * FROM Customer_Sales ;
+
+
+--------------------STEP 2. SET UP DATASETS--------------------
 
 -----Create JOIN TABLE-----
 CREATE TABLE Customer_Join (
@@ -132,10 +132,6 @@ Update Customer_Account
 SET CUSTOMER_NAME = (CUSTOMER_FIRST_NAME + ' ' + CUSTOMER_LAST_NAME)
 
 
-SELECT CUSTOMER_NAME
-FROM Customer_Account
-
-
 -----Clean WHITESPACE-----
 Update Customer_Sales
 SET CUSTOMER_NAME = TRIM(CUSTOMER_NAME)
@@ -159,94 +155,100 @@ WHERE CA.CUSTOMER_NAME = Customer_Sales.CUSTOMER_NAME) ;
 Update Customer_Sales
 SET CUSTOMER_NUMBER = null ;
  
------CREATE LEVENSCHTEIN FUNCTION-----
+
+--------------------STEP 3. LOAD IN LEVENSCHTEIN FUNCTION--------------------
+
+-----------------------------------------------------------------------------
+-----------------------CREATE LEVENSCHTEIN FUNCTION--------------------------
+
+	CREATE function LEVENSHTEIN ( @SourceString nvarchar(100), @TargetString nvarchar(100) ) 
+	--Returns the Levenshtein Distance between @SourceString string and @TargetString
+	--Translated to TSQL by Joseph Gama
+	--Updated slightly by Phil Factor
+	returns int
+	as
+	BEGIN
+	DECLARE @Matrix Nvarchar(4000), @LD int, @TargetStringLength int, @SourceStringLength int,
+	@ii int, @jj int, @CurrentSourceChar nchar(1), @CurrentTargetChar nchar(1),@Cost int, 
+	@Above int,@AboveAndToLeft int,@ToTheLeft int, @MinimumValueOfCells int
+	-- Step 1: Set n to be the length of s. Set m to be the length of t. 
+	--                    If n = 0, return m and exit.
+	--    If m = 0, return n and exit.
+	--    Construct a matrix containing 0..m rows and 0..n columns.
+	if @SourceString is null or @TargetString is null return null
+	Select @SourceStringLength=LEN(@SourceString), 
+		@TargetStringLength=LEN(@TargetString),
+		@Matrix=replicate(nchar(0),(@SourceStringLength+1)*(@TargetStringLength+1))
+	If @SourceStringLength = 0 return @TargetStringLength
+	If @TargetStringLength = 0 return @SourceStringLength
+	if (@TargetStringLength+1)*(@SourceStringLength+1)> 4000 return -1
+	--Step 2: Initialize the first row to 0..n.
+	--     Initialize the first column to 0..m.
+	SET @ii=0
+	WHILE @ii<=@SourceStringLength
+		BEGIN
+		SET @Matrix=STUFF(@Matrix,@ii+1,1,nchar(@ii))--d(i, 0) = i
+		SET @ii=@ii+1
+		END
+	SET @ii=0
+	WHILE @ii<=@TargetStringLength
+		BEGIN
+		SET @Matrix=STUFF(@Matrix,@ii*(@SourceStringLength+1)+1,1,nchar(@ii))--d(0, j) = j
+		SET @ii=@ii+1
+		END
+	--Step 3 Examine each character of s (i from 1 to n).
+	SET @ii=1
+	WHILE @ii<=@SourceStringLength
+		BEGIN
+	
+	--Step 4   Examine each character of t (j from 1 to m).
+		SET @jj=1
+		WHILE @jj<=@TargetStringLength
+			BEGIN
+	--Step 5 and 6
+			Select 
+			--Set cell d[i,j] of the matrix equal to the minimum of:
+			--a. The cell immediately above plus 1: d[i-1,j] + 1.
+			--b. The cell immediately to the left plus 1: d[i,j-1] + 1.
+			--c. The cell diagonally above and to the left plus the cost: d[i-1,j-1] + cost
+			@Above=unicode(substring(@Matrix,@jj*(@SourceStringLength+1)+@ii-1+1,1))+1,
+			@ToTheLeft=unicode(substring(@Matrix,(@jj-1)*(@SourceStringLength+1)+@ii+1,1))+1,
+			@AboveAndToLeft=unicode(substring(@Matrix,(@jj-1)*(@SourceStringLength+1)+@ii-1+1,1))
+			+ case when (substring(@SourceString,@ii,1)) = (substring(@TargetString,@jj,1)) 
+				then 0 else 1 end--the cost
+			-- If s[i] equals t[j], the cost is 0.
+		-- If s[i] doesn't equal t[j], the cost is 1.
+			-- now calculate the minimum value of the three
+			if (@Above < @ToTheLeft) AND (@Above < @AboveAndToLeft) 
+				select @MinimumValueOfCells=@Above
+		else if (@ToTheLeft < @Above) AND (@ToTheLeft < @AboveAndToLeft)
+				select @MinimumValueOfCells=@ToTheLeft
+			else
+				select @MinimumValueOfCells=@AboveAndToLeft
+			Select @Matrix=STUFF(@Matrix,
+					@jj*(@SourceStringLength+1)+@ii+1,1,
+					nchar(@MinimumValueOfCells)),
+			@jj=@jj+1
+			END
+		SET @ii=@ii+1
+		END    
+	--Step 7 After iteration steps (3, 4, 5, 6) are complete, distance is found in cell d[n,m]
+	return unicode(substring(
+	@Matrix,@SourceStringLength*(@TargetStringLength+1)+@TargetStringLength+1,1
+	))
+	END
+	go ;
 
 
-CREATE function LEVENSHTEIN ( @SourceString nvarchar(100), @TargetString nvarchar(100) ) 
---Returns the Levenshtein Distance between @SourceString string and @TargetString
---Translated to TSQL by Joseph Gama
---Updated slightly by Phil Factor
-returns int
-as
-BEGIN
-DECLARE @Matrix Nvarchar(4000), @LD int, @TargetStringLength int, @SourceStringLength int,
-@ii int, @jj int, @CurrentSourceChar nchar(1), @CurrentTargetChar nchar(1),@Cost int, 
-@Above int,@AboveAndToLeft int,@ToTheLeft int, @MinimumValueOfCells int
--- Step 1: Set n to be the length of s. Set m to be the length of t. 
---                    If n = 0, return m and exit.
---    If m = 0, return n and exit.
---    Construct a matrix containing 0..m rows and 0..n columns.
-if @SourceString is null or @TargetString is null return null
-Select @SourceStringLength=LEN(@SourceString), 
-     @TargetStringLength=LEN(@TargetString),
-     @Matrix=replicate(nchar(0),(@SourceStringLength+1)*(@TargetStringLength+1))
-If @SourceStringLength = 0 return @TargetStringLength
-If @TargetStringLength = 0 return @SourceStringLength
-if (@TargetStringLength+1)*(@SourceStringLength+1)> 4000 return -1
---Step 2: Initialize the first row to 0..n.
---     Initialize the first column to 0..m.
-SET @ii=0
-WHILE @ii<=@SourceStringLength
-    BEGIN
-    SET @Matrix=STUFF(@Matrix,@ii+1,1,nchar(@ii))--d(i, 0) = i
-    SET @ii=@ii+1
-    END
-SET @ii=0
-WHILE @ii<=@TargetStringLength
-    BEGIN
-    SET @Matrix=STUFF(@Matrix,@ii*(@SourceStringLength+1)+1,1,nchar(@ii))--d(0, j) = j
-    SET @ii=@ii+1
-    END
---Step 3 Examine each character of s (i from 1 to n).
-SET @ii=1
-WHILE @ii<=@SourceStringLength
-    BEGIN
- 
---Step 4   Examine each character of t (j from 1 to m).
-    SET @jj=1
-    WHILE @jj<=@TargetStringLength
-        BEGIN
---Step 5 and 6
-        Select 
-        --Set cell d[i,j] of the matrix equal to the minimum of:
-        --a. The cell immediately above plus 1: d[i-1,j] + 1.
-        --b. The cell immediately to the left plus 1: d[i,j-1] + 1.
-        --c. The cell diagonally above and to the left plus the cost: d[i-1,j-1] + cost
-        @Above=unicode(substring(@Matrix,@jj*(@SourceStringLength+1)+@ii-1+1,1))+1,
-        @ToTheLeft=unicode(substring(@Matrix,(@jj-1)*(@SourceStringLength+1)+@ii+1,1))+1,
-        @AboveAndToLeft=unicode(substring(@Matrix,(@jj-1)*(@SourceStringLength+1)+@ii-1+1,1))
-         + case when (substring(@SourceString,@ii,1)) = (substring(@TargetString,@jj,1)) 
-            then 0 else 1 end--the cost
-        -- If s[i] equals t[j], the cost is 0.
-      -- If s[i] doesn't equal t[j], the cost is 1.
-        -- now calculate the minimum value of the three
-        if (@Above < @ToTheLeft) AND (@Above < @AboveAndToLeft) 
-            select @MinimumValueOfCells=@Above
-      else if (@ToTheLeft < @Above) AND (@ToTheLeft < @AboveAndToLeft)
-            select @MinimumValueOfCells=@ToTheLeft
-        else
-            select @MinimumValueOfCells=@AboveAndToLeft
-        Select @Matrix=STUFF(@Matrix,
-                   @jj*(@SourceStringLength+1)+@ii+1,1,
-                   nchar(@MinimumValueOfCells)),
-           @jj=@jj+1
-        END
-    SET @ii=@ii+1
-    END    
---Step 7 After iteration steps (3, 4, 5, 6) are complete, distance is found in cell d[n,m]
-return unicode(substring(
-   @Matrix,@SourceStringLength*(@TargetStringLength+1)+@TargetStringLength+1,1
-   ))
-END
-go ;
-
-
+--------------------STEP 4. TEST LEVENSCHTEIN FUNCTION--------------------
 --TEST---
 SELECT dbo.LEVENSHTEIN('abcde', 'abCgF') ;
 
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
 
 
------APPLY LEVENSCHTEIN Function-----
+-------------STEP 5. USE LEVENSCHTEIN FUNCTION TO JOIN DATSETS------------
 
 --TRY--
 SELECT Customer_Account.CUSTOMER_NUMBER, Customer_Account.CUSTOMER_NAME, Customer_Sales.CUSTOMER_NAME,  
@@ -330,7 +332,7 @@ INSERT INTO Customer_Matched
 	ON U.CUSTOMER_NAME_CS = A.CUSTOMER_NAME_CS
 	AND U.PERCENTAGE_SIMILARITY = A.HIGHEST_PERCENTAGE ;
 
-SELECT * FROM Customer_Matched ;
+--SELECT * FROM Customer_Matched ;
 
 
 ------Check DUPLICATES-----
@@ -360,11 +362,11 @@ FROM Customer_Matched
 WHERE CUSTOMER_NAME_CS = CUSTOMER_NAME ;
 
 --CHECK--
-SELECT customer_number FROM Customer_Sales group by customer_number having count(*)>1 ; --no duplicates!
+--SELECT customer_number FROM Customer_Sales group by customer_number having count(*)>1 ; --no duplicates!
 
 
 --CHECK--
-SELECT * FROM Customer_Matched ;
+--SELECT * FROM Customer_Matched ;
 
 
 -----Join TABLES-----
